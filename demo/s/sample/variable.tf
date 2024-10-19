fe71c6972e56465c192444231d58a69d721b6a41
Warapong Plumjai <warapong.pj@gmail.com>
2024-10-19 15:22:08 +0700

    Create AWS S3 sample for demo on s

---
+++ demo/s/sample/variable.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "ap-southeast-1"
+}
+
+variable "project" {
+  default = "demo"
+}
+
+variable "environment" {
+  default = "s"
+}
+
+variable "name" {
+  default = "sample"
+}
+
+variable "acl" {
+  default = "private"
+}
+
+variable "owner" {
+  default = "group:default/bravo-team"
+}
