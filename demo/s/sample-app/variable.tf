d68e3bb106a070165a65d3b7314708dab2846ba2
Warapong Plumjai <warapong.pj@gmail.com>
2024-10-19 12:28:52 +0700

    Create AWS S3 sample-app for demo on s

---
+++ demo/s/sample-app/variable.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "ap-southeast-1"
+}
+
+variable "project" {
+  default = "demo"
+}
+
+variable "environment" {
+  default = "s"
+}
+
+variable "name" {
+  default = "sample-app"
+}
+
+variable "acl" {
+  default = "private"
+}
+
+variable "owner" {
+  default = "group:default/alpha-team"
+}
