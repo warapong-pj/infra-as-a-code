f7aeea00fd7ec9e10fe0577b6a6f4893c58abb10
warapong-pj <warapong.pj@gmail.com>
2024-10-26 09:14:53 +0700

    add step test deploy

---
+++ main.tf
@@ -0,0 +1,63 @@
+variable "region" {
+  default = "ap-southeast-1"
+}
+
+variable "project" {
+  default = "demo"
+}
+
+variable "environment" {
+  default = "d"
+}
+
+variable "name" {
+  default = "test"
+}
+
+variable "owner" {
+  default = "platform-team"
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = "MOCK_ACCESS_KEY"
+  secret_key = "MOCK_SECRET_KEY"
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    ec2 = "http://localhost:5000"
+    iam = "http://localhost:5000"
+    s3  = "http://localhost:5000"
+    sts = "http://localhost:5000"
+  }
+}
+
+locals {
+  tags = {
+    Name              = "${var.project}-${var.environment}-${var.name}"
+    Owner             = var.owner
+    Environment       = "SIT"
+    ManageByTerraform = "True"
+  }
+}
+
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.2"
+
+  bucket = "${var.project}-${var.environment}-${var.name}"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+
+  tags = local.tags
+}
